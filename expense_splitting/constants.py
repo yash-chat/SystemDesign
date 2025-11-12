6e3831705259682f23219e3af1243c24ac787b20
Yash Chaturvedi <71589899+yash-chat@users.noreply.github.com>
2025-11-12 20:32:22 +0530

    Delete expense_splitting directory

--- expense_splitting/constants.py
+++
@@ -1,8 +0,0 @@
-# Contains enums and constants used across the application.
-
-from enum import Enum
-
-class SplitType(Enum):
-    EQUAL = "EQUAL"
-    PERCENT = "PERCENT"
-    EXACT = "EXACT"
--- expense_splitting/expense_manager.py
+++
@@ -1,6 +0,0 @@
-from constants import *
-from user_manager import *
-
-UserManager().add_user("Yash", "901291040")
-print(UserManager().get_all_users())
-
